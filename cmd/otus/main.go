5145fedb808a4824fa1b3a37627cb6309e4b92fd
Evgeny Kemerov <eskemerov@gmail.com>
2020-02-13 21:59:11 +0300

    Add router and graceful shutdown

--- cmd/otus/main.go
+++ cmd/otus/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
+	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
 )
 
@@ -16,11 +22,24 @@ func main() {
 		logrus.Fatal("Port is not set")
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	router := mux.NewRouter()
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
-
 	})
 
-	http.ListenAndServe(":"+port, nil)
+	serv := http.Server{
+		Addr:    net.JoinHostPort("", port),
+		Handler: router,
+	}
 
+	go serv.ListenAndServe()
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	<-interrupt
+
+	timeout, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelFunc()
+	serv.Shutdown(timeout)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/sudoguy/otus-webinar
 
 go 1.13
 
-require github.com/sirupsen/logrus v1.4.2
+require (
+	github.com/gorilla/mux v1.7.4 // indirect
+	github.com/sirupsen/logrus v1.4.2
+)
